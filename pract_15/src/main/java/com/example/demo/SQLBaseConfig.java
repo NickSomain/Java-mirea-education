114e36306d28a1ca9be3521230233c963596ae60
NickSomain <90514291+NickSomain@users.noreply.github.com>
2022-05-31 16:43:56 +0300

    Delete pract_15 directory

--- pract_15/src/main/java/com/example/demo/SQLBaseConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public class SQLBaseConfig {
-    public static final String DB_USERNAME="postgres";
-    public static final String DB_PASSWORD="admin";
-    public static final String DB_URL="jdbc:postgresql://localhost:5432/java";
-}

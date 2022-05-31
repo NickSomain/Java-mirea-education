6f7d3b3b9372cfe8460ee5c333a300146db3e17c
NickSomain <90514291+NickSomain@users.noreply.github.com>
2022-05-31 16:44:12 +0300

    Delete pract_16-24 directory

--- pract_16-24/src/main/java/com/example/demo/Aspect/LogExecutionTime.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Aspect;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface LogExecutionTime {
-}
--- pract_16-24/src/main/java/com/example/demo/Service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Service;
-
-
-public interface EmailService {
-    void sendSimpleMessage(String text);
-}
